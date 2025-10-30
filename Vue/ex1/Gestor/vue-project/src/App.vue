7343ccba1312c3d05f6d8b263bf28acf6f6ba602
N0mbreP <pol.paredes.7e7@itb.cat>
2025-10-30 17:44:44 +0100

    Hola

---
+++ Vue/ex1/Gestor/vue-project/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+const tascaNova= ref('')
+
+</script>
+
+<template>
+  <h1>Gestor de tasques</h1>
+  <input v-model="tascaNova" placeholder="Tasca nova" />
+  <div>
+    
+  </div>
+</template>
+
+<style scoped></style>
---
+++ Vue/ex1/vue-project/src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './ExempleU.vue'
+import ExempleU from './ExempleU.vue'
+
+createApp(App).mount('#app')
+//createApp(ExempleU).mount('#exmpleu')
