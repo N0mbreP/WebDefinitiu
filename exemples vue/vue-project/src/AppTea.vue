41b508f839adaf41ae122e455fe6c5cec68efa4d
N0mbreP <pol.paredes.7e7@itb.cat>
2025-11-06 17:05:12 +0100

    Funciona

--- exemples vue/vue-project/src/AppTea.vue
+++ exemples vue/vue-project/src/AppTea.vue
@@ -1,17 +1,39 @@
-<script setup lang="ts">
-import Tes from './components/Tes.vue';
+<script setup>
+import { ref } from "vue";
+import Tea from "./components/Tea.vue";
 
-const llistatTe= ["Verd","Vermell","Negre","Blanc"]
+const teas = [
+  { name: "Verds", description: "Te verd suau i fresc", price: 2.5 },
+  { name: "Negres", description: "Te negre intens i robust", price: 3.0 },
+  { name: "Herbes", description: "Infusió de herbes naturals", price: 2.0 },
+  { name: "Blanc", description: "Infusió de herbes aromàtiques", price: 2.0 },
+  { name: "Vermell", description: "Infusió de fruites del bosc", price: 2.0 },
+];
+
+// Variable reactiva per a guardar el te seleccionat
+const selectedTea = ref("");
+
+// Función que rep l' event del fill
+const handleEnviarNom = (name) => {
+  selectedTea.value = name;
+};
 </script>
 
 <template>
-    <div class="content">
-        <h1 class="title">Subscripcions disponibles</h1>
-        <h2 class="subtitle">
-            Disposem dels millor tes del món
-        </h2>
-        <div class="plans">
-           <Tes v-for="te in llistatTe":key="te" :name="te"/>
-        </div>
+  <div class="content">
+    <h1 class="title">Subscripcions disponibles</h1>
+    <h2 class="subtitle">Disposem dels millor tes del món</h2>
+
+     <!-- Te seleccionat en div -->
+    <div v-if="selectedTea" class="selected-tea" :class="{ highlighted: selectedTea }">
+      {{ selectedTea }}
     </div>
+
+    <div class="plans">
+    <!-- escoltem event -->
+      <Tea :teas="teas" @enviarNom="handleEnviarNom" />
+    </div>
+  </div>
 </template>
+
+<style scoped></style>
---
+++ exemples vue/vue-project/src/components/Tea.vue
@@ -0,0 +1,33 @@
+<script setup>
+// què escolto? els prop
+const props = defineProps({
+  teas: {
+    type: Array,
+    required: true,
+  },
+});
+// què envio? els emits
+// Definim emits que el component pot enviar
+// emit és una funció que ens inventem i que permet enviar events al component pare
+const emit = defineEmits(["enviarNom"]);
+// Funció que emet el nom del te seleccionat
+const agafarNom = (name) => {
+  emit("enviarNom", name);
+};
+
+</script>
+
+<template>
+<div class="plans">
+    <div class="plan" v-for="tea in props.teas" :key="index" @click="agafarNom(tea.name) ">
+      <div class="description">
+        <span class="title">{{ tea.name }}</span>
+        <span>{{ tea.description }}</span>
+        <span>{{ tea.price }} €</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
